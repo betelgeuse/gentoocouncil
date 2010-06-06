3c5d24bed70a160e0a976ca68611db9f55b67ed8
Wei Xie <xieconnect@gmail.com>
2010-06-06 09:44:56 +0800

    Cucumber configuration

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,6 +26,3 @@ config.action_mailer.delivery_method = :test
 # This is necessary if your schema can't be completely dumped by the schema dumper,
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
-
-# Make it run faster with in memory db
-config.gem 'lda-memory_test_fix', :lib => 'memory_test_fix'
