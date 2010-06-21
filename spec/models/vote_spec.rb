8efa5ad6b672635b97420e6e5363748febea8387
Wei Xie <xieconnect@gmail.com>
2010-06-21 23:36:06 +0800

    rename councilmember to council_member in spec

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Vote do
   it "should allow voting only once" do
     q = questions(:question)
-    u = users(:councilmember)
+    u = users(:council_member)
     v = q.votes.build(:support => false, :reasoning => 'bad', :user => u)
     v.should be_valid
     v.creatable_by?(u).should be_true
